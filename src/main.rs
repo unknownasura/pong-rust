b34c730123ac80e252ee129793e95a1b329e137d
Ashura <150961150+unknownasura@users.noreply.github.com>
2024-03-16 00:14:34 -0300

    Add tetra crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use tetra::graphics::{self, Color};
+use tetra::{Context, ContextBuilder, State};
+
+struct GameState {}
+
+impl State for GameState {}
+
+fn main() -> tetra::Result {
+    ContextBuilder::new("Pong", 640, 480)
+        .quit_on_escape(true)
+        .build()?
+        .run(|_| Ok(GameState {}))
 }
